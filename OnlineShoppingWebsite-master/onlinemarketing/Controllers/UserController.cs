9644bcd77643613bda7d052b697d1a97bc4e0992
Syed Asif <syedashamaila1721@gmail.com>
2022-04-11 19:16:22 +0530

    First Commit

---
+++ OnlineShoppingWebsite-master/onlinemarketing/Controllers/UserController.cs
@@ -0,0 +1,310 @@
+﻿using onlinemarketing.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace onlinemarketing.Controllers
+{
+    public class UserController : Controller
+    {
+        onlinemarketingEntities db = new onlinemarketingEntities();
+
+        // GET: User
+        public ActionResult Index(int ? page)
+        {
+
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.cateogories.Where(x => x.cat_status == 1).OrderByDescending(x => x.cat_id).ToList();
+            IPagedList<cateogory> cate = list.ToPagedList(pageindex, pagesize);
+
+            return View(cate);
+            
+        }
+
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Register(tbl_user us, HttpPostedFileBase imgfile)
+        {
+
+            string path = uploadimage(imgfile);
+
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "image could not be uploaded";
+
+            }
+            else
+            {
+                tbl_user u = new tbl_user();
+                u.u_name = us.u_name;
+                u.u_password = us.u_password;
+                u.u_contact = us.u_contact;
+                u.u_email = us.u_email;
+                u.u_image = path;
+                db.tbl_user.Add(u);
+                db.SaveChanges();
+
+
+
+                return RedirectToAction("Login");
+
+            }
+                return View();
+        }
+
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tbl_user svm)
+        {
+
+            tbl_user ad = db.tbl_user.Where(x => x.u_email == svm.u_email && x.u_password == svm.u_password).SingleOrDefault();
+            if (ad != null)
+            {
+                Session["u_id"] = ad.u_id.ToString();
+                Session["user"] = ad.u_name;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.error = "Invalid User Name or Password";
+            }
+
+            
+
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public ActionResult CreateAdd()
+        {
+            List<cateogory> li = db.cateogories.ToList();
+            ViewBag.categorylist = new SelectList(li, "cat_id", "cat_name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAdd(product p , HttpPostedFileBase imgfile)
+        {
+            List<cateogory> li = db.cateogories.ToList();
+            ViewBag.categorylist = new SelectList(li, "cat_id", "cat_name");
+
+
+            string path = uploadimage(imgfile);
+
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "image could not be uploaded";
+
+            }
+            else
+
+            {
+
+
+                product pr = new product();
+                pr.pro_name = p.pro_name;
+                pr.pro_price = p.pro_price;
+                pr.pro_image = path;
+                pr.cat_id_fk = p.pro_user_id_fk;
+
+                pr.pro_desc = p.pro_desc;
+                pr.pro_user_id_fk = Convert.ToInt32(Session["u_id"].ToString());
+                db.products.Add(pr);
+                db.SaveChanges();
+
+                Response.Redirect("Index");
+            }
+                return View();
+        }
+
+
+
+        public ActionResult DisplayAdd(int ? id, int?page)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.products.Where(x => x.cat_id_fk==id).OrderByDescending(x => x.pro_id).ToList();
+            IPagedList<product> cate = list.ToPagedList(pageindex, pagesize);
+
+            return View(cate);
+        }
+
+
+        [HttpPost]
+        public ActionResult DisplayAdd(int? id, int? page,string search)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.products.Where(x=>x.pro_name.Contains(search)).OrderByDescending(x => x.pro_id).ToList();
+            IPagedList<product> cate = list.ToPagedList(pageindex, pagesize);
+
+            return View(cate);
+        }
+        // image upload ............................
+
+
+
+
+        public string uploadimage(HttpPostedFileBase file)
+
+        {
+
+            Random r = new Random();
+
+            string path = "-1";
+
+            int random = r.Next();
+
+            if (file != null && file.ContentLength > 0)
+
+            {
+
+                string extension = Path.GetExtension(file.FileName);
+
+                if (extension.ToLower().Equals(".jpg") || extension.ToLower().Equals(".jpeg") || extension.ToLower().Equals(".png"))
+
+                {
+
+                    try
+
+                    {
+
+
+
+                        path = Path.Combine(Server.MapPath("~/Content/upload"), random + Path.GetFileName(file.FileName));
+
+                        file.SaveAs(path);
+
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
+
+
+
+                        //    ViewBag.Message = "File uploaded successfully";
+
+                    }
+
+                    catch (Exception ex)
+
+                    {
+
+                        path = "-1";
+
+                    }
+
+                }
+
+                else
+
+                {
+
+                    Response.Write("<script>alert('Only jpg ,jpeg or png formats are acceptable....'); </script>");
+
+                }
+
+            }
+
+
+
+            else
+
+            {
+
+                Response.Write("<script>alert('Please select a file'); </script>");
+
+                path = "-1";
+
+            }
+
+
+
+
+
+
+
+            return path;
+
+
+        }
+
+
+
+
+
+
+
+        //end
+
+
+        public ActionResult SignOut()
+        {
+
+            
+            Session.RemoveAll();
+            Session.Abandon();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Add_Delete(int ? id )
+        {
+            product p = db.products.Where(x => x.pro_id == id).SingleOrDefault();
+            db.products.Remove(p);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult ViewAdds(int ? id  )
+        {
+
+            ad_view_model adm = new ad_view_model();
+
+            product p = db.products.Where(x => x.pro_id == id).SingleOrDefault();
+            adm.pro_id = p.pro_id;
+            adm.pro_name = p.pro_name;
+            adm.pro_image = p.pro_image;
+            adm.pro_price = p.pro_price;
+            adm.pro_desc = p.pro_desc;
+
+            cateogory cat = db.cateogories.Where(x => x.cat_id == p.cat_id_fk).SingleOrDefault();
+            adm.cat_name = cat.cat_name;
+            tbl_user u = db.tbl_user.Where(x => x.u_id == p.pro_user_id_fk).SingleOrDefault();
+            adm.u_name = u.u_name;
+            adm.u_image = u.u_image;
+            adm.u_contact = u.u_contact;
+            adm.pro_user_id_fk = u.u_id;
+            return View (adm);
+
+
+            
+        }
+
+
+        }
+    }
---
+++ OnlineShoppingWebsite-master/onlinemarketing/Models/ad_view_model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace onlinemarketing.Models
+{
+    public class ad_view_model
+    {
+        public int pro_id { get; set; }
+        public string pro_name { get; set; }
+        public string pro_image { get; set; }
+        public Nullable<int> pro_price { get; set; }
+        public string pro_desc { get; set; }
+
+
+
+        public Nullable<int> cat_id_fk { get; set; }
+        public Nullable<int> pro_user_id_fk { get; set; }
+
+        public int cat_id { get; set; }
+        public string cat_name { get; set; }
+
+
+        public string u_name { get; set; }
+
+     
+    public string u_image { get; set; }
+
+        public string u_contact { get; set; }
+
+
+
+    }
+}
